a2bb42ad568d86bc4b471f1216633fc41646e23d
Russ <rudavis@gmail.com>
2015-11-23 21:04:29 -0800

    Adding and removing Rows works.  ng-show & hide table too

--- js/MeetingCost.js
+++ js/MeetingCost.js
@@ -1,7 +1,9 @@
 angular.module("MeetingCost", []).controller("MeetingCostController",
     function MeetingCostController($scope, $filter) {
-
+        $scope.rates = [];
+        $scope.totalRate = 0;
         $scope.calculatorDisplay = 0;
+        $scope.hasTotal = false;
 
         $scope.onClickClear = function () {
             $scope.calculatorDisplay = 0;
@@ -11,6 +13,22 @@ angular.module("MeetingCost", []).controller("MeetingCostController",
                 $scope.calculatorDisplay = btn;
             } else {
                 $scope.calculatorDisplay += String(btn);
+            }
+        };
+        $scope.updateTotal = function (num) {
+            $scope.totalRate += num;
+        };
+        $scope.onClickPlus = function () {
+            $scope.rates.push(Number($scope.calculatorDisplay));
+            $scope.updateTotal(Number($scope.calculatorDisplay));
+            $scope.hasTotal = true;
+            $scope.onClickClear();
+        };
+        $scope.deleteRow = function (rowNum) {
+            $scope.totalRate -= $scope.rates[rowNum];
+            $scope.rates.splice(rowNum, 1);
+            if ($scope.rates.length == 0) {
+                $scope.hasTotal = false;
             }
         };
     });
